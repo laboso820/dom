ce9746641e75199837fbc274560054ce6ec8e2e9
laboso820 <obadiahlaboso820@gmail.com>
2025-04-08 19:12:58 +0300

    dom

---
+++ main.js
@@ -0,0 +1,8 @@
+document.title= "dom manipulation"
+// console.log("this from script")
+// const headingEl=document.getElementById("heading")
+// headingEl.innerHTML("document object model")
+const heading=document.getElementById("heading")
+// console.log(typeof document.getElementById("heading"))
+document.querySelector("heading")
+heading.innerHTML="dom manipulation"
