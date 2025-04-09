f6fd8d78e91fc9139d03a20295523226c663ec98
laboso820 <obadiahlaboso820@gmail.com>
2025-04-09 21:31:32 +0300

    styles added

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-document.title= "dom manipulation"
+document.title = "dom manipulation";
+const body = document.getElementById("hero");
+const buttonEl = document.getElementById("btn");
+const buttonEl2 = document.getElementById("btn2");
+const buttonEl3 = document.getElementById("btn3");
 // console.log("this from script")
 // const headingEl=document.getElementById("heading")
 // headingEl.innerHTML("document object model")
-const heading=document.getElementById("heading")
+const heading = document.getElementById("heading");
 // console.log(typeof document.getElementById("heading"))
-document.querySelector("heading")
-heading.innerHTML="dom manipulation"
+document.querySelector("heading");
+heading.innerHTML = "dom manipulation";
+// body.innerHTML=" lorem ipsum nmnm"
+buttonEl.addEventListener("click", function () {
+  body.style.color = "red";
+});
+buttonEl2.addEventListener("click", function () {
+  body.style.color = "green";
+});
+buttonEl3.addEventListener("click", function () {
+  body.style.color = "blue";
+});
