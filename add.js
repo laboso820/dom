07bd0b6825ea5cf4cf4965aabc2d33d6467a0e8d
laboso820 <obadiahlaboso820@gmail.com>
2025-04-09 22:26:34 +0300

    new changes

---
+++ add.js
@@ -0,0 +1,6 @@
+console.log("add file display")
+const number1=document.getElementById("num1")
+const number2=document.getElementById("num2")
+const buttonEl1=number1+number2
+buttonEl1.getElementById("para")
+
